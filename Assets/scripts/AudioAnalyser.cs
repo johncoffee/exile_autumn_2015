376ecd78c95947bc87b98f34e622721084192325
KM <system_KMP@yahoo.com>
2015-11-06 23:07:11 +0100

    shaky player

---
+++ Assets/scripts/AudioAnalyser.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class AudioAnalyser : MonoBehaviour {
+
+    private AudioSource _as;
+    private float[] _spectrumData = new float[512];
+
+    [Range(0.0f, 1.0f)]
+    public float TrackTime;
+
+    private static AudioAnalyser _instance;
+
+    public static AudioAnalyser Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        _instance = this;
+        _as = GetComponent<AudioSource>();
+    }
+
+	
+	void Update () {
+        if (Mathf.Abs(TrackTime - _as.time / _as.clip.length) > 0.01f)
+        {
+            _as.time = TrackTime * _as.clip.length;
+        }
+
+        _as.GetSpectrumData(_spectrumData, 0, FFTWindow.Rectangular);
+        TrackTime = _as.time / _as.clip.length;
+
+    }
+
+    public float RetrievePeakOfData(int startIndex, int endIndex)
+    {
+        return _spectrumData.ToList().GetRange(startIndex, endIndex - startIndex).Max();
+    }
+}
--- Assets/scripts/CameraPostProcess.cs
+++ Assets/scripts/CameraPostProcess.cs
@@ -14,8 +14,24 @@ public class CameraPostProcess : ImageEffectBase
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         material.SetTexture("_GlitchTexture", GlitchTexture);
+        if (Application.isPlaying)
+        {
+            float amount = AudioAnalyser.Instance.RetrievePeakOfData(0, 20) * 100.0f;
+            if (amount > 55.0f)
+            {
+                ShakeFrequency = 1.0f;
+                ShakeAmount = (amount - 20.0f) / 5.0f;
+
+            }
+            else
+            {
+                ShakeFrequency = 0.0f;
+            }
+        }
+
         material.SetFloat("_ShakeFrequency", ShakeFrequency);
         material.SetFloat("_ShakeAmount", ShakeAmount);
+        
         Graphics.Blit(source, destination, material);
     }
 
